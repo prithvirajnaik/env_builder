7f988820cdb09e34e4148a19f23a20e8925844df
Prithviraj <prithvirajnaik318@gmail.com>
2025-10-02 21:14:51 +0530

    update check

--- setup.ps1
+++ setup.ps1
@@ -5,7 +5,7 @@ param (
 
 function Update-Setup {
     $setupUrl = "https://raw.githubusercontent.com/prithvirajnaik/env_builder/refs/heads/main/setup.ps1"
-    $setupPath = "$PSScriptRoot\setup.ps1"
+    $setupPath = "C:\Scripts\setup.ps1"
     Invoke-WebRequest -Uri $setupUrl -OutFile $setupPath -UseBasicParsing
     Write-Host "Setup script updated!"
 }

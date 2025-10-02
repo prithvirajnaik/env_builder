96297039d82440bcbd3cc338db8321d3a008be42
Prithviraj <prithvirajnaik318@gmail.com>
2025-10-02 21:00:55 +0530

    error fixed , typo

--- setup.ps1
+++ setup.ps1
@@ -44,7 +44,7 @@ switch ($tool.ToLower()) {
     "git"    { Install-Git }
     "vscode" { Install-VSCode }
     "all"    { Install-Python; Install-Cpp; Install-Git; Install-VSCode }
-    "update" { update-setup }
+    "update" { Update-setup }
     default { Write-Host "Unknown tool: $tool. Available: python, cpp, git, vscode, all" }
 }
 
